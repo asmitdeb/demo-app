bcabf8e4c59bfc11fcc189654cc172390a0a0223
Asmit Deb <47671715+asmitdeb@users.noreply.github.com>
2024-04-26 05:45:37 +0530

    added recipes

--- app.js
+++ app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const path = require('path');
 const collection = require('./connect');
+const axios = require('axios');
 
 const app = express();
 const port = 3000;
+const api_key = "6a7dd33727124df5b48a846b35796aac";
 
 app.use(express.json());
 
-app.use(express.urlencoded({extended: false})); //needed?
+
+
+// app.use(express.static('public'))
+
+app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('views', path.join(__dirname, 'views'));
-app.set("view engine", "hbs");
+// app.set("view engine", "hbs");
+app.set('view engine', 'ejs');
 
 
 app.get("/", (req, res) => {
@@ -33,7 +40,7 @@ app.post("/signup", async (req, res) => {
     
     if(userExists){
         // res.sendFile('public/accexists.html', {root: './'})
-        res.render('response.hbs',{
+        res.render('response.ejs',{
             title: "User Exists",
             response: "Account already exists.",
             loc: "/",
@@ -44,7 +51,7 @@ app.post("/signup", async (req, res) => {
         const userdata = await collection.insertMany(data);
         console.log(userdata);
         // res.sendFile('public/accreated.html', {root: './'})
-        res.render('response.hbs',{
+        res.render('response.ejs',{
             title: "Account created",
             response: "Account created successfully!",
             loc: "/",
@@ -60,7 +67,7 @@ app.post("/login", async (req, res) => {
         const check = await collection.findOne({email: req.body.email});
         if(!check){
             // res.sendFile('public/notfound.html', {root: './'});
-            res.render('response.hbs',{
+            res.render('response.ejs',{
                 title: "Not found",
                 response: "User not found",
                 loc: "/signup",
@@ -71,16 +78,17 @@ app.post("/login", async (req, res) => {
         const equals = (req.body.pass === check.password);
         if(equals){
             // res.sendFile('public/home.html', {root: './'})
-            res.render('response.hbs',{
-                title: "Home",
-                response: "Logged in successfully!",
-                loc: "/",
-                btn: "Logout"
-            });
+            // res.render('response.hbs',{
+            //     title: "Home",
+            //     response: "Logged in successfully!",
+            //     loc: "/",
+            //     btn: "Logout"
+            // });
+            res.render("index.ejs");
         }
         else{
             // res.sendFile('public/wrongpass.html', {root: './'})
-            res.render('response.hbs',{
+            res.render('response.ejs',{
                 title: "Wrong pass",
                 response: "Wrong password",
                 loc: "/",
@@ -89,7 +97,7 @@ app.post("/login", async (req, res) => {
         }}
     }catch{
         // res.sendFile('public/error.html', {root: './'})
-        res.render('response.hbs',{
+        res.render('response.ejs',{
             title: "Error",
             response: "Error",
             loc: "/",
@@ -97,6 +105,22 @@ app.post("/login", async (req, res) => {
         });
     }
 });
+
+//Search
+app.post('/search', async (req, res) => {
+    const {query} = req.body;
+    const response = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?query=${query}&apiKey=${api_key}`)
+    const recipes = response.data.results
+    res.render('results.ejs', {recipes})
+})
+
+//Recipe
+app.get("/recipe/:id", async(req,res )=> {
+    const {id} = req.params;
+    const response = await axios.get(`https://api.spoonacular.com/recipes/${id}/information?apiKey=${api_key}`)
+    const recipe = response.data;
+    res.render('recipe.ejs', {recipe})
+})
 
 app.listen(port, () => {
     console.log('Server running on port: ${port}');
